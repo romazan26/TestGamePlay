4a576a76d9703b2e614ce247b681ffffb5110a8a
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-12-05 15:48:01 +0300

    add setting view

---
+++ TestGamePlay/SettingUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingUIView.swift
+//  TestGamePlay
+//
+//  Created by Роман on 05.12.2023.
+//
+
+import SwiftUI
+
+struct SettingUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingUIView()
+}
