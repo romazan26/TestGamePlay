d73e9494ea11fa6b3bf153791e0a5c998bae4343
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-12-05 13:29:22 +0300

    blur

---
+++ TestGamePlay/CustonProgressBarView.swift
@@ -0,0 +1,18 @@
+//
+//  CustonProgressBarView.swift
+//  TestGamePlay
+//
+//  Created by Роман on 05.12.2023.
+//
+
+import SwiftUI
+
+struct CustonProgressBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustonProgressBarView()
+}
