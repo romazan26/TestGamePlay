67ed412f9293573d4c0a47633b6ac9f04aef0712
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-12-05 23:47:48 +0300

    add sound effect

---
+++ TestGamePlay/Sound/SoudManager.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  TestGamePlay
+//
+//  Created by Роман on 05.12.2023.
+//
+
+import Foundation
+import AVKit
+
+final class SoundManager {
+    
+    static let shared = SoundManager()
+    
+    var player: AVAudioPlayer?
+    
+    func playSound() {
+        
+    }
+}
