20f5624ad785ad5ba8ae2df1aa61dba86e78f364
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-12-07 23:38:55 +0300

    add shopView

---
+++ TestGamePlay/screen/ShopUIView.swift
@@ -0,0 +1,18 @@
+//
+//  ShopUIView.swift
+//  TestGamePlay
+//
+//  Created by Роман on 07.12.2023.
+//
+
+import SwiftUI
+
+struct ShopUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShopUIView()
+}
