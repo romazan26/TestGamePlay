5ddeb0af703cca88089645c6e8f69dab71fc2a8c
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-12-09 16:58:09 +0300

    Good work shop

---
+++ TestGamePlay/StorekitManager.swift
@@ -0,0 +1,8 @@
+//
+//  StorekitManager.swift
+//  TestGamePlay
+//
+//  Created by Роман on 09.12.2023.
+//
+
+import Foundation
--- TestGamePlay/ViewModelShop.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ViewModelShop.swift
-//  TestGamePlay
-//
-//  Created by Роман on 08.12.2023.
-//
-
-import Foundation
